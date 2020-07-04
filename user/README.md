99c0b5a618866613409164adf022ebe407689d56
Leonardo Grandi <leonardofelipegrandi@gmail.com>
2020-07-03 23:55:46 -0300

    Refactor

---
+++ user/README.md
@@ -0,0 +1,4 @@
+docker-compose -f docker-compose.yml up
+
+Mostrar tabelas do banco ->
+aws dynamodb list-tables --endpoint-url http://127.0.0.1:8000
---
+++ user/src/app.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
+const implementation = require('./implementation');
+
+// Schema dos contratos de comunicação;
+const packageDefinition = protoLoader.loadSync(
+  path.resolve(__dirname, 'proto', 'user_messages.proto'), {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true
+});
+
+const proto = grpc.loadPackageDefinition(packageDefinition);
+
+// Criação do server gRPC;
+const server = new grpc.Server();
+
+// Injeção dos controllers no ProtoBuffer;
+server.addService(proto.UserService.service, implementation);
+server.bind('0.0.0.0:3334', grpc.ServerCredentials.createInsecure());
+console.log('Running server 0.0.0.0:3334');
+server.start();
